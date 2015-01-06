bdc54e4209f4b47a3ee666f046b81c8d98b74fbe
jameslew <jameslew@live.com>
2015-01-05 16:36:17 -0800

    bug fix package.json

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
+//var routes = require('./routes');
 var http = require('http');
 var path = require('path');
 var raspBBQ = require('./routes/raspBBQ.js');
